01d5ae6fc764836b64465c349839fa0517a286c3
manuelatrotta <manuelat88@hotmail.it>
2020-01-17 11:29:15 +0100

    correzione

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //variabile che incluse gli <a> presenti nella classe hamburger-menu
-var hamburgerMenu = $('.hamburger-menu a');
+var hamburgerMenu = $('.hamburger-menu > a');
 //evento:click
 //funzione in cui nascondo la var hamburgerMenu con modalità slow, ma si può mettere anche valore in ms!
 hamburgerMenu.click(
@@ -8,9 +8,9 @@ hamburgerMenu.click(
   }
 );
 
-//creo una variabile nascondi e all'evento click dichiarando nascondi= true si manifesta l'evento show con modalità slow
+//creo una variabile nascondi e all'evento click dichiarando nascondi= true si manifesta l'evento show con modalità slow. Devo selezionare solo le a figli del div header-right
 var nascondi;
-$('.header-right > a').click(
+$('.header-right > a > i').click(
  function() {
   if (nascondi = true) {
     $(".hamburger-menu").show('slow');
